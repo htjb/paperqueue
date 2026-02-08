1e7fb47cea3b9abf81f03e7101a9c00ce50202c4
Harry Bevins <harrytjbevins@yahoo.co.uk>
2026-02-08 15:51:28 +0000

    format the code

--- paperqueue/commands/read.py
+++ paperqueue/commands/read.py
@@ -1,15 +1,15 @@
 """Mark a paper as read."""
+import argparse
 
 from paperqueue.utils import load_queue, move_paper, save_queue
 
 
-def read(args):
+def read(args: argparse.Namespace) -> None:
     """Move paper from queue to archive."""
-
     data = load_queue()
 
-    data['queue'], data['archive'] = (
-        move_paper(args.id, data['queue'], data['archive'])
+    data["queue"], data["archive"] = move_paper(
+        args.id, data["queue"], data["archive"]
     )
 
     save_queue(data)
--- paperqueue/utils.py
+++ paperqueue/utils.py
@@ -1,40 +1,50 @@
 """Util functions."""
-import numpy as np
+
 import json
 from pathlib import Path
 
+import numpy as np
+
 QUEUE_FILE = Path.home() / ".paperqueue.json"
 
-def load_queue():
+
+def load_queue() -> dict:
+    """Load the catalogue of papers."""
     if not QUEUE_FILE.exists():
         return {"queue": [], "archive": [], "next_id": 1}
     return json.loads(QUEUE_FILE.read_text())
 
-def save_queue(data):
+
+def save_queue(data: dict) -> None:
+    """Resave the catalogue.
+    
+    Args:
+        data: the catalogue.
+    """
     QUEUE_FILE.write_text(json.dumps(data, indent=2))
 
-def is_already_added(data, arxiv_id):
+
+def is_already_added(data: dict, arxiv_id: str) -> tuple[bool, str | None]:
+    """Check is paper is already in the queue or archive."""
     # Check queue
     if any(paper["arxiv_id"] == arxiv_id for paper in data["queue"]):
         return True, "queue"
-    
+
     # Check archive
     if any(paper["arxiv_id"] == arxiv_id for paper in data["archive"]):
         return True, "archive"
-    
+
     return False, None
 
-def move_paper(id, a, b):
+def move_paper(id: int, a: list, b: list) -> tuple[list, list]:
     """Move a paper from list a to b.
-    
+
     Args:
         id: the papers id.
         a: list a e.g. queue.
         b: list b e.g. archive.
     """
-
-    index = np.argmax([elem["id"] == id
-                       for elem in a])
+    index = np.argmax([elem["id"] == id for elem in a])
     paper = a[index]
     del a[index]
     b.append(paper)
